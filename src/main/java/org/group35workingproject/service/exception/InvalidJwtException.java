9d87cbc49a76b7b7c37ce0eda5ca407b45bb12e4
Mihail Nedioglo <nmg84nmg@gmail.com>
2024-04-19 09:10:24 +0200

    update

---
+++ src/main/java/org/group35workingproject/service/exception/InvalidJwtException.java
@@ -0,0 +1,8 @@
+package org.group35workingproject.service.exception;
+
+public class InvalidJwtException extends RuntimeException {
+
+    public InvalidJwtException(String message) {
+        super(message);
+    }
+}
